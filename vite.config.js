4092e283c1cd47817316780993ef4493d1d5f2ee
AsepSaniNugraha <asepsaninugraha@gmail.com>
2026-03-06 22:06:45 +0700

    Configure base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dark-light-mode-authform/',
   plugins: [react(), tailwindcss()],
 })
